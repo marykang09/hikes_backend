c316ffde7e418125133d037950f54a7152b22623
lmonty22 <lindsay.montgomery@olympian.org>
2020-05-26 12:50:24 -0400

    working on adding hikes

--- app/controllers/hikes_controller.rb
+++ app/controllers/hikes_controller.rb
@@ -1,2 +1,4 @@
 class HikesController < ApplicationController
+
+   
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+
+    def show
+        user = User.find(params[:id])
+         render json: user.as_json(include: {hikes: {
+                                        include: {trail: {
+                                            only: [:name, :location, :img_medium, :id]}},
+                                        except: [:created_at, :updated_at]
+                                            }})
+       
+    end
+    
 end
